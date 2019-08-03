ba11f399e5f8db02ce8a36dc501c2187d377b757
Alija Bobija <bobija.alija@gmail.com>
2019-08-03 12:37:08 +0200

    C# fetch data from Mysql tutorial

---
+++ fetch_data_from_mysql_using_csharp/MySqlConnTest/MySqlConnTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlConnTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = string.Format(
+                    "server={0};uid={1};pwd={2};database={3}",
+                    "localhost",
+                    "root",
+                    "00000000",
+                    "wiattend"
+                );
+
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (var cmd = new MySqlCommand("SELECT first_name, last_name FROM tag", conn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            var fname = reader.GetString(0);
+                            var lname = reader.GetString(1);
+
+                            Console.WriteLine($"{fname} {lname}");
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ fetch_data_from_mysql_using_csharp/README.md
@@ -0,0 +1,5 @@
+# C# fetch data from MySql database
+
+## YouTube
+
+[![C# fetch data from MySql database](https://img.youtube.com/vi/Die4mKMQ1_8/hqdefault.jpg)](https://www.youtube.com/watch?v=Die4mKMQ1_8)
