da652ae88a491bde4f559216555ed4d81f3d0413
Alija Bobija <bobija.alija@gmail.com>
2019-08-05 12:59:23 +0200

    C# insert data into MySQL

---
+++ insert_data_into_mysq_using_csharp/MySQLInsertDemo/MySQLInsertDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLInsertDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uid;
+            string fname;
+            string lname;
+
+            Console.Write("UID: ");
+            uid = Console.ReadLine();
+
+            Console.Write("First Name: ");
+            fname = Console.ReadLine();
+
+            Console.Write("Last Name: ");
+            lname = Console.ReadLine();
+
+            var connectionString = string.Format(
+                    "server={0};uid={1};pwd={2};database={3}",
+                    "localhost",
+                    "root",
+                    "00000000",
+                    "wiattend"
+                );
+
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO tag(uid, first_name, last_name) " +
+                        "VALUES (@uid, @fname, @lname)";
+
+                    cmd.Parameters.AddWithValue("@uid", uid);
+                    cmd.Parameters.AddWithValue("@fname", fname);
+                    cmd.Parameters.AddWithValue("@lname", lname);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ insert_data_into_mysq_using_csharp/README.md
@@ -0,0 +1,5 @@
+# C# Insert Into MySQL
+
+## YouTube
+
+[![C# Insert Into MySQL](https://img.youtube.com/vi/srTKOJyPM84/hqdefault.jpg)](https://www.youtube.com/watch?v=srTKOJyPM84)
